5ab066c1cc0e0555154aed140056b11c0b69b4fd
Your Name <abhimishra.db12@gmail.com>
2025-04-13 20:43:00 +0530

    ff

--- backend/models/SubCategory.js
+++ backend/models/SubCategory.js
@@ -4,7 +4,46 @@ const subCategorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   mainCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'MainCategory' },
   description: String,
-  image: String
+  image: String,
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    value: Number,
+    totalReviews: Number,
+  },
+  contact: {
+    phone: String,
+    email: String,
+    whatsappAvailable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  businessHours: {
+    monday: String,
+    tuesday: String,
+    wednesday: String,
+    thursday: String,
+    friday: String,
+    saturday: String,
+    sunday: String,
+  },
+  aboutBusiness: {
+    type: String,
+  },
+  serviceDetails: {
+    whatsIncluded: [String],
+    whatYouNeed: [
+      {
+        title: String,
+        note: String,
+      },
+    ],
+  },
+  
 });
 
 export default mongoose.model('SubCategory', subCategorySchema);
